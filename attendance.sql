257d26d6649c09ebca0f5866f2359b4495048108
hahirwaraissa <hahirwa014@gmail.com>
2024-11-01 13:54:08 +0200

    created procedure

---
+++ attendance.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PROCEDURE Calculate_Attendance_Statistics (
+    p_month IN NUMBER,
+    p_year IN NUMBER
+) IS
+    -- Variables to hold employee details
+    v_employee_id     NUMBER;
+    v_first_name      VARCHAR2(50);
+    v_last_name       VARCHAR2(50);
+    
+    -- Variables to count attendance
+    v_total_presents  NUMBER;
+    v_total_absents   NUMBER;
+    v_attendance_days NUMBER;
+    v_attendance_percentage NUMBER;
